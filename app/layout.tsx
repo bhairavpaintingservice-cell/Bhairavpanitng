959a876d0bf3bd8690a8e3e28b3533f3073339b7
bhairavpaintingservice-cell <bhairavpaintingservice@gmail.com>
2026-03-28 14:24:17 +0530

    Integrate Google Tag Manager in layout
    Added Google Tag Manager script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import WhatsAppFloat from "@/components/whatsapp-float";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -52,8 +52,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en"><Script id="gtm-head" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K58KMSPL');`}}/>
+      <body className={inter.className}><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K58KMSPL" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         <Header />
         <main>{children}</main>
         <Footer />
